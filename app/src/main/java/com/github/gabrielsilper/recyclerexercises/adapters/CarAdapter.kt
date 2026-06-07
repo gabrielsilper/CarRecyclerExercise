5442fd6fa8feb12b987156c902105dc0051a4afb
gabrielsilper <gabrielsilper.main@gmail.com>
2026-06-07 12:00:20 -0400

    feat: criar o Adapter para itens de carros

---
+++ app/src/main/java/com/github/gabrielsilper/recyclerexercises/adapters/CarAdapter.kt
@@ -0,0 +1,43 @@
+package com.github.gabrielsilper.recyclerexercises.adapters
+
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.ImageView
+import android.widget.TextView
+import androidx.recyclerview.widget.RecyclerView
+import com.github.gabrielsilper.recyclerexercises.R
+import com.github.gabrielsilper.recyclerexercises.models.Car
+import java.text.NumberFormat
+import java.util.Locale
+
+class CarAdapter(val cars: List<Car>) : RecyclerView.Adapter<CarAdapter.CarViewHolder>() {
+    override fun onCreateViewHolder(
+        parent: ViewGroup,
+        viewType: Int
+    ): CarViewHolder {
+        val view = LayoutInflater.from(parent.context).inflate(R.layout.car_item, parent, false)
+        return CarViewHolder(view)
+    }
+
+    override fun onBindViewHolder(holder: CarViewHolder, position: Int) {
+        val car = cars[position]
+        holder.bind(car)
+    }
+
+    override fun getItemCount(): Int {
+        return this.cars.size
+    }
+
+    class CarViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
+        fun bind(car: Car) {
+            itemView.findViewById<ImageView>(R.id.carImage).setImageResource(car.image)
+            itemView.findViewById<TextView>(R.id.carModel).text = car.model
+            itemView.findViewById<TextView>(R.id.carBrand).text = car.brand
+            itemView.findViewById<TextView>(R.id.carYear).text = car.year.toString()
+
+            val brlFormat = NumberFormat.getCurrencyInstance(Locale("pt", "BR"))
+            itemView.findViewById<TextView>(R.id.carPrice).text = brlFormat.format(car.price)
+        }
+    }
+}
