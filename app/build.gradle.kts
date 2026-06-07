6ce962d8fd69e367bc2685908fbee76864a267dc
gabrielsilper <gabrielsilper.main@gmail.com>
2026-06-07 12:27:45 -0400

    fix: ajusta SDK do projeto para normalizar erro de compilação

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -4,16 +4,12 @@ plugins {
 
 android {
     namespace = "com.github.gabrielsilper.recyclerexercises"
-    compileSdk {
-        version = release(36) {
-            minorApiLevel = 1
-        }
-    }
+    compileSdk = 37
 
     defaultConfig {
         applicationId = "com.github.gabrielsilper.recyclerexercises"
         minSdk = 24
-        targetSdk = 36
+        targetSdk = 37
         versionCode = 1
         versionName = "1.0"
 
